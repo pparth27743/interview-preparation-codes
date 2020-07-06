b4788c1e0647a9e980b6ae384046d27c489baf85
Parth Patel <62556601+pparth277@users.noreply.github.com>
2020-07-06 17:20:38 +0530

    Create sumOfDigits.cpp

---
+++ sumOfDigits.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int n){ 
+    
+    int sum=0;
+    while(n!=0){
+        sum += (n%10);
+        n = n/10;
+    }
+    return sum;
+} 
+
+int main() {
+    
+    std::cout << sumOfDigits(123456) << std::endl;
+    
+	return 0;
+}
