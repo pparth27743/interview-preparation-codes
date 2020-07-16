76a551933564d296e4810ec9dc7f5e28fbb9478e
Parth Patel <62556601+pparth27743@users.noreply.github.com>
2020-07-16 16:32:30 +0530

    Update Sieve_of_Eratosthenes.cpp

--- Sieve_of_Eratosthenes.cpp
+++ Sieve_of_Eratosthenes.cpp
@@ -1,41 +1,49 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void sieve_of_prime(int arr[], int N){
+vector<int> sieve(int N){
+    
+    bool isPrime[N+1] = {false};
+    vector<int> v;
     
    // Mark all the odd numbers as prime 
-    for(int i=3; i<N; i+=2)
-        arr[i] = 1;
+    for(int i=3; i<N+1; i+=2)
+        isPrime[i] = true;
         
     // Sieve of Eratosthenes
-    for(int i=3; i*i<N; i+=2){
-        if(arr[i] == 1){
+    for(int i=3; i*i<N+1; i+=2){
+        if(isPrime[i] == 1){
             for(int j=i*i; j<N; j+=i)
-                arr[j] = 0;
+                isPrime[j] = false;
         }
     }
         
     // Special cases
-    arr[0] = 0;
-    arr[1] = 0;
-    arr[2] = 1;
+    isPrime[0] = false;
+    isPrime[1] = false;
+    isPrime[2] = true;
+    
+    
+    // make isPrimeay of prime numbers
+    for(int i=0; i<N; i++)
+        if(isPrime[i] == true)
+            v.push_back(i);
+            
+    return v;
     
 }
 
 
-
 int main(){
     
-    int N = 100;
-    int arr[N] = {0};
+    int N = 30;
     
-    sieve_of_prime(arr, N);
+    vector<int> v = sieve(N);
     
     cout << "List of prime numbres upto " << N << ": ";
     
-    for(int i=0; i<N; i++)
-        if(arr[i] == 1)
-            cout << i << " ";
+    for(auto i=v.begin(); i!=v.end(); i++)
+        cout << *i << " ";   
 
     return 0;
 }
