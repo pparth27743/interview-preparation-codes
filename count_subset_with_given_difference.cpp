8d5e7cc487c9997ff4b1cb56840d9ccad813712d
Parth Patel <62556601+pparth27743@users.noreply.github.com>
2020-12-31 09:57:30 +0530

    Update count_subset_with_given_difference.cpp

--- count_subset_with_given_difference.cpp
+++ count_subset_with_given_difference.cpp
@@ -14,7 +14,7 @@ int countSubsetSum(int arr[], int n, int sum){
     
     for(int i=1; i<=n; i++){
         for(int j=1; j<=sum; j++){
-            if(arr[i-1]<=sum)
+            if(arr[i-1]<=j)
                 dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]];
             else
                 dp[i][j] = dp[i-1][j];
