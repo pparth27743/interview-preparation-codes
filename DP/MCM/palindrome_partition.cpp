97adbf25c3cd23a060c5c47a8a1b8089c12172d0
Parth Patel <pparth27743@gmail.com>
2021-06-09 13:51:22 +0530

    added

--- DP/MCM/palindrome_partition.cpp
+++ DP/MCM/palindrome_partition.cpp
@@ -1,6 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
+// O(n^2) 
+int minCut(string s) {
+        
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        
+        // here we have to initialize
+        // dp[0] = -1
+        
+        for(int i=0; i<=n; i++)
+            dp[i] = i-1;
+        
+        for(int i=0; i<n; i++){
+            
+            // odd length palindrome
+            for(int j=0; i-j>=0 && i+j<n && s[i-j] == s[i+j]; j++)
+                dp[i+j+1] = min(dp[i+j+1], 1 + dp[i-j]);
+            
+            // even length palindrome
+            for(int j=0; i-j>=0 && i+j+1<n && s[i-j] == s[i+j+1]; j++)
+                dp[i+j+2] = min(dp[i+j+2], 1 + dp[i-j]);
+            
+        }
+        
+        return dp[n];
+        
+}
+
+
+// O(n^3)
 int isPalindrome(string s, int i, int j, vector<vector<int> > &p){
 
     if(p[i][j] != -1)
@@ -14,7 +46,6 @@ int isPalindrome(string s, int i, int j, vector<vector<int> > &p){
     }
     return p[i][j] = 1;
 }
-
 
 int solve(string s, int i, int j, vector<vector<long> > &dp, vector<vector<int> > &p){
 
@@ -45,6 +76,8 @@ int main()
     vector<vector<int> > p(n+1, vector<int> (n+1, -1));
 
     cout << solve(s, 0, n-1, dp, p) << endl;
+
+    cout << minCut(s) << endl;
 
     return 0;
 }
