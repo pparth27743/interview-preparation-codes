c0ddb024e17a46c781864a03114a43ffaf88b319
Parth Patel <62556601+pparth27743@users.noreply.github.com>
2024-09-23 21:41:17 +0530

    Update minimum_subset_sum_difference.cpp

--- DP/KnapSack/minimum_subset_sum_difference.cpp
+++ DP/KnapSack/minimum_subset_sum_difference.cpp
@@ -1,3 +1,5 @@
+// Problem Statement: https://www.geeksforgeeks.org/partition-a-set-into-two-subsets-such-that-the-difference-of-subset-sums-is-minimum/
+
 #include <bits/stdc++.h>
 using namespace std;
 
